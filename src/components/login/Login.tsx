bb08cde81d256dc8efeaf6a5897a8e21f4d68bb5
bokdol11859 <2019147551@yonsei.ac.kr>
2022-12-06 00:20:11 +0900

    fix: Fix scrollable div

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { GoogleIcon, KakaoIcon } from '../global/Icons';
 import { motion } from 'framer-motion';
+import SPACING from 'constants/Spacing';
 
 type Props = {};
 
 const Login = (props: Props) => {
   return (
-    <Wrapper initial={{ y: 250, opacity: 0 }} transition={{ duration: 1 }} whileInView={{ opacity: 1, y: 40 }}>
+    <Wrapper initial={{ y: 200, opacity: 0 }} transition={{ duration: 1 }} whileInView={{ opacity: 1, y: 0 }}>
       <LoginButtons>
         <KakaoIcon />
         <GoogleIcon />
+        <CompanyName>c. myplanit</CompanyName>
       </LoginButtons>
       <BackgroundImage src="/assets/LoginLogo.png" />
     </Wrapper>
@@ -19,10 +21,11 @@ const Login = (props: Props) => {
 
 const Wrapper = styled(motion.div)`
   width: 100%;
-  height: 93vh;
+  height: 100vh;
   display: flex;
   align-items: flex-end;
   justify-content: center;
+  padding-bottom: 40px;
 `;
 
 const LoginButtons = styled.div`
@@ -30,15 +33,25 @@ const LoginButtons = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 10px;
+  gap: ${SPACING.SPACING400}px;
+`;
+
+const CompanyName = styled.p`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 21px;
+  margin-top: ${SPACING.SPACING400}px;
 `;
 
 const BackgroundImage = styled.img`
   position: fixed;
-  bottom: -100px;
+  bottom: 0px;
   width: 110%;
   max-width: 450px;
   z-index: -1;
+  overflow: hidden;
 `;
 
 export default Login;

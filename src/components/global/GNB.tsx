b6d138fbb3ac1942fa8dbb170382b5f4098f7635
bokdol11859 <2019147551@yonsei.ac.kr>
2022-12-06 16:26:34 +0900

    refactor: Refactor GNB

--- src/components/global/GNB.tsx
+++ src/components/global/GNB.tsx
@@ -4,38 +4,43 @@ import SPACING from '../../../constants/Spacing';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { MypageIcon, TodoBoxIcon, TodoMallIcon } from './Icons';
+import { PATHS } from '../../../constants/Paths';
 
 type Props = {};
 
 const GNB = (props: Props) => {
-  const router = useRouter();
+  const { replace, asPath } = useRouter();
 
-  const handleClick = (route: string) => {
-    router.replace(route);
+  const handleClick = (path: string) => {
+    replace(path);
+  };
+
+  const checkPath = (path: string) => {
+    return asPath === path;
   };
 
   return (
     <NavBar>
       <Column
         onClick={() => {
-          handleClick('/todobox');
+          handleClick(PATHS.TODOBOX);
         }}>
-        <TodoBoxIcon selected={router.asPath === '/todobox'} />
-        <Title active={router.asPath === '/todobox'}>투두함</Title>
+        <TodoBoxIcon selected={checkPath(PATHS.TODOBOX)} />
+        <Title active={checkPath(PATHS.TODOBOX)}>투두함</Title>
       </Column>
       <Column
         onClick={() => {
-          handleClick('/todomall');
+          handleClick(PATHS.TODOMALL);
         }}>
-        <TodoMallIcon selected={router.asPath === '/todomall'} />
-        <Title active={router.asPath === '/todomall'}>투두몰</Title>
+        <TodoMallIcon selected={checkPath(PATHS.TODOMALL)} />
+        <Title active={checkPath(PATHS.TODOMALL)}>투두몰</Title>
       </Column>
       <Column
         onClick={() => {
-          handleClick('/mypage');
+          handleClick(PATHS.MYPAGE);
         }}>
-        <MypageIcon selected={router.asPath === '/mypage'} />
-        <Title active={router.asPath === '/mypage'}>내 기록</Title>
+        <MypageIcon selected={checkPath(PATHS.MYPAGE)} />
+        <Title active={checkPath(PATHS.MYPAGE)}>내 기록</Title>
       </Column>
     </NavBar>
   );
@@ -45,9 +50,9 @@ const NavBar = styled.footer`
   position: fixed;
   display: flex;
   align-items: center;
-  height: 56px;
-  padding-top: 1px;
-  padding-bottom: 34px;
+  height: 3.5rem;
+  padding-top: 0.06rem;
+  padding-bottom: 2.125rem;
   margin: 0 auto;
   width: 100%;
   max-width: ${SPACING.MAXWIDTH}px;
@@ -60,22 +65,21 @@ const NavBar = styled.footer`
 
 const Column = styled.div`
   text-align: center;
-  border: none;
   display: flex;
   width: 100%;
-  height: 48px;
+  height: 3rem;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 4px;
+  gap: 0.25rem;
 `;
 
 const Title = styled.p<{ active: boolean }>`
   font-family: 'Pretendard';
   font-style: normal;
   font-weight: 400;
-  font-size: 10px;
-  line-height: 12px;
+  font-size: 0.625rem;
+  line-height: 0.75rem;
   color: ${(props) => (props.active ? `${THEME.PURPLE500}` : `${THEME.GRAY400}`)};
 `;
 
